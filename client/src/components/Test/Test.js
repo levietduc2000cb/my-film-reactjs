eba4aa5ec72052704d39e383652a720d860b168d
LeVietDuc2000 <levietduc2000cb@gmail.com>
2022-05-11 22:25:16 +0700

    upload source code

---
+++ client/src/components/Test/Test.js
@@ -0,0 +1,9 @@
+import NotificationSuccess from "../Notification/NotificationSuccess/NotificationSuccess";
+function Test(props) {
+  return (
+    <div>
+      <NotificationSuccess />
+    </div>
+  );
+}
+export default Test;
